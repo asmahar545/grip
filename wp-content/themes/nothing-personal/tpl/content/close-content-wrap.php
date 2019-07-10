7764cbcc57883a2e6892b9075582568479f3fc79
Manirakiza <mj.manirakiza@grip.org>
2019-07-10 15:46:27 +0200

    commit 4

--- wp-content/themes/nothing-personal/tpl/content/close-content-wrap.php
+++
@@ -1,4 +0,0 @@
-<?php get_template_part('tpl/generic/search-modal'); ?>
-</div>
-</div>
-<?php do_action('nothing_personal_after_content_wrapper');?>
--- wp-content/themes/nothing-personal/tpl/copyright/close-copyright-wrap.php
+++
@@ -1,4 +0,0 @@
-<?php do_action( 'nothing_personal_after_copyright_content' ); ?>
-</div>
-</div>
-</div>
